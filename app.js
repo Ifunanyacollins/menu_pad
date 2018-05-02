7692ce3efa0ad4183805edc40f2c692b1e4f1dca
collins <collinsogbuzuru@gmail.com>
2018-05-02 11:22:54 +0000

    update

---
+++ app.js
@@ -0,0 +1,21 @@
+var express = require("express"),
+    app = express();
+    
+    
+    
+app.set("view engine", "ejs");
+app.use(express.static("public"));
+  
+ 
+ app.get("/", function(req, res) {
+    res.render("index");
+}); 
+  
+ app.get("/home", function(req, res) {
+    res.render("home");
+});  
+  
+
+
+app.listen(process.env.PORT, process.env.IP);
+  
